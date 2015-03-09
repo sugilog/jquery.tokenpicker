7c045445da488a6887fb27489044596c07af58a4
sugilog <sugilog@gmail.com>
2015-03-09 19:20:09 +0900

    move sources into src directory and output to dist by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,17 @@ var wrapTemplate,
     concat = require( "gulp-concat" ),
     wrap   = require( "gulp-wrap" );
 
-gulp.task( "default", [] );
+gulp.task( "js", function() {
+  return gulp
+    .src( "./src/javascript/jquery.tokenpicker.js" )
+    .pipe( concat( "jquery.tokenpicker.js" ) )
+    .pipe( gulp.dest( "./dist/javascript/" ) );
+});
+
+gulp.task( "css", function() {
+  return gulp
+    .src( "./src/stylesheet/jquery.tokenpicker.css" )
+    .pipe( gulp.dest( "./dist/stylesheet/" ) );
+});
+
+gulp.task( "default", [ "js", "css" ] );
